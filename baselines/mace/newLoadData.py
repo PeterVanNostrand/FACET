Corrected coverage calculation for GraphMerge

baselines/mace/newLoadData.py
@@ -97,9 +97,9 @@ VALID_MUTABILITY_TYPES = {
     True,
     False}
 
-RANDOM_SEED = 54321
-seed(RANDOM_SEED)  # set the random seed so that the random permutations can be reproduced again
-np.random.seed(RANDOM_SEED)
+# RANDOM_SEED = 54321
+# seed(RANDOM_SEED)  # set the random seed so that the random permutations can be reproduced again
+# np.random.seed(RANDOM_SEED)
 
 
 class Dataset(object):
@@ -507,11 +507,16 @@ class Dataset(object):
             if preprocessing is not None:
                 assert with_meta == False, 'This feature is not built yet...'
 
+            # X_train, X_test, y_train, y_test = train_test_split(
+            #     all_data,
+            #     all_true_labels,
+            #     test_size=.2,
+            #     random_state=RANDOM_SEED)
+
             X_train, X_test, y_train, y_test = train_test_split(
                 all_data,
                 all_true_labels,
-                test_size=.2,
-                random_state=RANDOM_SEED)
+                test_size=.2)
 
             # ordering of next two lines matters (shouldn't overwrite input_cols); silly code... :|
             U_train = X_train[self.getMetaAttributeNames()]
@@ -526,11 +531,16 @@ class Dataset(object):
             all_data = balanced_data_frame.loc[:, input_cols]
             all_true_labels = balanced_data_frame.loc[:, output_col]
 
+            # X_train, X_test, y_train, y_test = train_test_split(
+            #     all_data,
+            #     all_true_labels,
+            #     train_size=.7,
+            #     random_state=RANDOM_SEED)
+
             X_train, X_test, y_train, y_test = train_test_split(
                 all_data,
                 all_true_labels,
-                train_size=.7,
-                random_state=RANDOM_SEED)
+                train_size=.7)
 
             # TODO (2020.05.18): this should be updated so as NOT to update meta variables
             if preprocessing == 'standardize':

utilities/metrics.py
@@ -62,10 +62,10 @@ def classification_metrics(preds, y, verbose=True):
     out_string = ""
 
     # Compute performance metrics
-    tp = np.where((preds == 0) & (y == 0))[0].sum()  # true inliers
-    fp = np.where((preds == 0) & (y == 1))[0].sum()  # false inliers
-    tn = np.where((preds == 1) & (y == 1))[0].sum()  # true outliers
-    fn = np.where((preds == 1) & (y == 0))[0].sum()  # false outlier
+    tp = np.sum((preds == 1) & (y == 1))  # true outliers
+    fp = np.sum((preds == 1) & (y == 0))  # false outliers
+    tn = np.sum((preds == 0) & (y == 0))  # true inliers
+    fn = np.sum((preds == 0) & (y == 1))  # false inliers
 
     accuracy = (tp + tn) / (tp + fp + tn + fn)
     precision = tp / (tp + fp)
@@ -107,7 +107,7 @@ def average_distance(x, xprime, distance_metric="Euclidean"):
     xprime = xprime[~idx_bad_examples]
 
     if xprime.shape[0] == 0:
-        print("No valid examples found")
+        # print("No valid examples found")
         avg_dists = np.nan
     else:
         dists = distance_fn(x, xprime)