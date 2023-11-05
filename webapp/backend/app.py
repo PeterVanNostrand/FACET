0196a3153763574ed555d1190ecc2f67265c828d
Randy Huang <huangyulu.zh@gmail.com>
2023-11-04 21:51:16 -0400

    add more server stuff

--- webapp/backend/app.py
+++ webapp/backend/app.py
@@ -1,39 +1,60 @@
 from flask import Flask, request, jsonify
 
+import os
+import sys
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+facet_dir = os.path.dirname(os.path.dirname(current_dir))
+sys.path.append(facet_dir)
+from main import flask_run
+from dataset import load_data
+
 app = Flask(__name__)
 
-# intialize components
-def initialize_components():
-    # Load dataset
-    # Train machine learning model
-    # Initialize FACET index
-    return None
+manager = None
+
+
+def test_manager():
+    x, y = load_data("vertebral", preprocessing="Normalize")
+    print(x, y)
+
+    test_data = {"property_name": "sample_property"}
+
+    result = manager.explainer.explain(test_data)
+    print("Manager test result:", result)
+
 
 @app.before_first_request
 def initialize_app():
-    initialize_components()
+    print("Initializing app...")
+    current_directory = os.getcwd()
+    print("Current Working Directory:", current_directory)
+
+    manager = flask_run()
+    print("Manager initialized")
+
+    test_manager()
 
 
-
-@app.route('/')
+@app.route("/")
 def index():
-    return 'Welcome to the Facet web app!'
+    return "Welcome to the Facet web app!"
 
-@app.route('/facet/explanation', methods=['POST'])
+
+@app.route("/facet/explanation", methods=["POST"])
 def facet_explanation():
-    # Get the data from the request
     data = request.get_json()
-
-    # Process the data (you can replace this with your logic)
     result = process_facet_data(data)
 
     return jsonify(result)
 
+
 def process_facet_data(data):
-    # Data is a JSON object received in the request
+    # JSON object received in the request
     # data['property_name']
-    
+
     return data
 
-if __name__ == '__main__':
-    app.run()
+
+if __name__ == "__main__":
+    app.run(debug=True)
