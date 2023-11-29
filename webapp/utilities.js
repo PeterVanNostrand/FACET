1996691b1ecedc21448d52731d805a1876c6ec77
HoneyJar-0 <76642194+HoneyJar-0@users.noreply.github.com>
2023-11-29 14:06:18 -0500

    added utilities file, refactored code to refer to utilities

---
+++ webapp/utilities.js
@@ -0,0 +1,49 @@
+// utilities.js
+
+// Frontend Utility Functions
+
+/**
+ * Takes in either a pascal case or snake case string and formats it with spaces
+ * @param {String} str - Input string to format
+ * @returns {String} - formatted string. (ex: BananaPudding_In_Salami -> Banana Pudding In Salami)
+ */
+function formatString(str){
+    const regex = /[A-Z][a-z]*/g;
+    const matchArray = [...str.matchAll(regex)];
+    return(matchArray.join(' '));
+}
+
+/**
+ * Takes in some float/int and formats it to be more human readable (Thanks Peter!)
+ * @param {String} featureValue - Input number as string
+ * @param {String} featureName - Input name of the feature
+ * @param {Object} readable - Input dictionary of units
+ * @returns {String} - formatted string. (ex: BananaPudding_In_Salami -> Banana Pudding In Salami)
+ */
+function formatNumerical(featureValue, featureName, readable) {
+    var valueText = "";
+    var trimmedValue = parseFloat(featureValue).toFixed(readable["feature_decimals"][featureName])
+    if (readable["feature_units"][featureName] == "$") {
+        valueText += formatter.format(trimmedValue);
+    }
+    else {
+        valueText += trimmedValue + " " + readable["feature_units"][featureName];
+    }
+    return valueText
+}
+  // Backend Utility Functions
+  
+  /**
+   * Function to perform a task on the backend.
+   * @param {any} data - Input data for the task.
+   * @returns {any} - Result of the task.
+   */
+  
+  // Export the functions if using in a Node.js environment
+  // You can also use a module system if applicable (e.g., CommonJS, ES modules)
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = {
+        formatString,
+        formatNumerical
+    };
+  }
