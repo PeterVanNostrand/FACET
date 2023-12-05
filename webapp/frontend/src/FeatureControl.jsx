50b4679ed1611738f14fb05cf5b038de84fb6904
bfgen <bfgenin@wpi.edu>
2023-12-05 16:28:54 -0500

    in-progress updating for review, not ready yet

--- webapp/frontend/src/FeatureControl.jsx
+++ webapp/frontend/src/FeatureControl.jsx
@@ -1,50 +1,85 @@
 // FeatureControl.jsx
-import React, { useState } from 'react'; // Don't forget to import useState
-import NumberLine1 from './NumberLine1.jsx';
+import React, { useState } from 'react'; 
+import NumberLineChart from  './NumberLineChart.jsx';
 import triangleSVG from './SVG/Triangle.svg';
+import filltriangleSVG from './SVG/FillTriangle.svg'
 import lockSVG from './SVG/Lock.svg';
 import unlockSVG from './SVG/UnLocked.svg';
+import pinSVG from './SVG/Pinned.svg';
+import unpinSVG from './SVG/UnPinned.svg';
 
-const FeatureControl = ({ name, priority}) => {
-  const [newPriority, setNewPriority] = useState(priority);
-  const [isLocked, setIsLocked] = useState(false);
-  const handleTriangleUpClick = () => {
-    // Logic for on click
-   //console.log('Triangle Up Clicked!');
-  };
 
-  const handleTriangleDownClick = () => {
-    // logic for on click 
-    //console.log('Triangle Down Clicked!');
-  };
+const FeatureControl = ({ id, name, priority, onUpdatePriority, lockState}) => {
+  const [currPriority, setNewPriority] = useState(priority);
+  const [isLocked, setIsLocked] = useState(lockState);
+  const [isPinned, setIsPinned] = useState(false);
+
+ // Handle Locking, Pinning and Arrows (priority)
+ const handleTriangleUpClick = () => {
+  if (!isPinned && currPriority !== 1) {
+    console.log('up pressed');
+    onUpdatePriority(id, currPriority,-1);
+  }
+};
+
+const handleTriangleDownClick = () => {
+  if (!isPinned) {
+    console.log('down pressed');
+    onUpdatePriority(id, currPriority, 1);
+  }
+};
+
 
  const handleLockClick = () => {
     // Toggle between locked and unlocked
     setIsLocked((prevIsLocked) => !prevIsLocked);
-    //console.log('Lock Clicked!');
+    console.log('Lock Clicked!');
+    console.log(name);
   };
 
+  
+ const handlePinClick = () => {
+  // Toggle between pinned and unpinned
+  setIsPinned((prevIsLocked) => !prevIsLocked);
+  console.log('Pin Clicked!');
+};
+
   return (
-    <div className='feature-control-box'>
+    <div className={`feature-control-box ${isLocked ? 'locked' : ''}`}>
       <h1 className='feature-title'>{name}</h1>
-      {/* Arrow Triangles*/}
-      <div className='lock'>
+      <div className='lock'> 
         <img
-        src={isLocked ? lockSVG : unlockSVG}
-        alt={isLocked ? 'Lock' : 'Unlock'}
-        onClick={handleLockClick}
-        style={{ filter: isLocked ? 'none' : 'grayscale(100%)', color: isLocked ? '' : '#636363'}}
+          src={isLocked ? lockSVG : unlockSVG}
+          alt={isLocked ? 'Lock' : 'Unlock'}
+          onClick={handleLockClick}
+          className={isLocked ? 'locked' : ''}
         />
       </div>
-      <div className='triangle-container' style={{width: '25px', height: '25px'}}>
-      <img src={triangleSVG} alt='Triangle up'
-       style={{marginLeft: '23px', marginTop: '-5px', color: isLocked? 'none': 'black'}} onClick={handleTriangleUpClick} />
-      <img src={triangleSVG} alt='Triangle down' style={{marginLeft: '23px', marginTop: '25px', transform: 'rotate(180deg)', color: isLocked? 'none': 'black'}} onClick={handleTriangleDownClick} />
+      <div className='pin'>
+        <img
+          src={isPinned ? pinSVG : unpinSVG}
+          alt={isPinned ? 'Pin' : 'UnPin'}
+          onClick={handlePinClick}
+          className={isPinned ? 'pinned' : ''}
+        />
       </div>
-      <div className='priority-value'>{newPriority}</div>
+      <div className='triangle-container'>    {/* Arrow Triangles*/}
+        <img
+          src={isPinned ? filltriangleSVG : triangleSVG}
+          alt='Triangle up'
+          onClick={handleTriangleUpClick}
+        />
+        <img
+          src={isPinned ? filltriangleSVG : triangleSVG}
+          alt='Triangle down'
+          onClick={handleTriangleDownClick}
+          style={{marginTop: 45, transform: 'rotate(180deg)'}} 
+        />
+      </div>
+      <div className='priority-value'>{currPriority}</div>
       <div className='number-line'>
+      <NumberLineChart start={0} end={100} initialMinRange={34} initialMaxRange={50} currentValue={54} isLocked/>
       </div>
-        <NumberLine1 initialMinValue={30} initialMaxValue={50} initialCurValue={40} minX={0} maxX={150}/>
     </div>
   );
 };
