b43ce0139c8544096dfd8fc02ca5c2ec8c43150e
Randy Huang <huangyulu.zh@gmail.com>
2023-12-06 17:50:57 -0500

    minor refactors

--- webapp/frontend/src/components/explanation/NumberLine.jsx
+++ webapp/frontend/src/components/explanation/NumberLine.jsx
@@ -1,35 +1,34 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect } from 'react';
 import { select } from 'd3';
-import { numberLineBuilder } from '../../js/numberLineBuilder'
+import { numberLineBuilder } from '../../js/numberLineBuilder';
 
 const NumberLine = ({ explanation }) => {
-    const numberLineRef = useRef();
-
-
     useEffect(() => {
-        const selection = select(numberLineRef.current);
-        const visual_display = numberLineBuilder(selection);
-        selection.append('circle')
-            .attr('cx', 50)
-            .attr('cy', 50)
-            .attr('r', 20)
-            .style('fill', 'red');
-        
-        var svg = select('#svg_container').append("svg")
-            .attr('width', 800)
-            .attr('height', 375)
-            .attr("fill", "white")
-            .attr("id", "image_svg");
+        // Check if the svg element already exists
+        const svgContainer = select('#svg_container');
+        const existingSvg = svgContainer.select('svg');
 
-        svg.call(visual_display)
+        // Use the existing svg or create a new one
+        const svg = existingSvg.empty()
+            ? svgContainer.append('svg')
+                .attr('width', 800)
+                .attr('height', 375)
+                .attr('fill', 'white')
+                .attr('id', 'image_svg')
+            : existingSvg;
+            
 
-    }, []);
+        const visualDisplay = numberLineBuilder(explanation);
+        svg.call(visualDisplay);
+
+        // Clean up when the component unmounts
+        return () => {
+            svg.selectAll('*').remove();
+        };
+    }, [explanation]);
 
     return (
-        <div>
-            {/* <svg ref={numberLineRef} width="100" height="100"></svg> */}
-            <div id="svg_container"></div>
-        </div>
+        <div id="svg_container"></div>
     );
 };
 
