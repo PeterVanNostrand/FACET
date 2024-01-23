b44e05dba85fd3e3f862529f7fbb16672a4bb477
PeterVanNostrand <petermvannostrand@gmail.com>
2024-01-23 17:35:11 -0500

    missing imports commented out

--- webapp/frontend/src/main.jsx
+++ webapp/frontend/src/main.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './css/index.css'
 import WelcomeScreen from './WelcomeSceen.jsx'
-import NumberLine from './NumberLine.jsx'
-import FeatureControl from './FeatureControlTab.jsx'
+import './css/index.css'
+// import FeatureControl from './FeatureControlTab.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <WelcomeScreen/>
+        <WelcomeScreen />
     </React.StrictMode>,
 )
