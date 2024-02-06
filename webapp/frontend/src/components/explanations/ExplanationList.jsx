d3ec434c5403fbfbfd27db5cf0386c419b481fe0
Randy Huang <huangyulu.zh@gmail.com>
2024-02-06 01:08:01 -0500

    fix explanation index mismatch btwn single and multiple

--- webapp/frontend/src/components/explanations/ExplanationList.jsx
+++ webapp/frontend/src/components/explanations/ExplanationList.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import NumberLine from './NumberLine';
 import { formatFeature } from "../../js/utilities.js";
 
 const ExplanationList = ({ explanations, totalExplanations, featureDict, formatDict }) => {
     const [currentExplanationIndex, setCurrentExplanationIndex] = useState(0);
+
 
     const handleNext = () => {
         setCurrentExplanationIndex((prevIndex) => (prevIndex + 1) % explanations.length);
@@ -14,16 +15,21 @@ const ExplanationList = ({ explanations, totalExplanations, featureDict, formatD
     };
 
     return (
-        <div className="explanations-container" style={{ margin: 10, flex: 1, overflowY: 'auto' }}>
+        <div
+            className="explanations-container"
+            style={{ marginBottom: 10, marginTop: 15 }}
+        >
             <div>
-                {Object.keys(explanations[currentExplanationIndex]).map((key, innerIndex) => (
+                {Object.keys(
+                    explanations[explanations.length === 1 ? 0 : currentExplanationIndex]
+                ).map((key, innerIndex) => (
                     <div key={innerIndex} style={{ display: 'flex', flexDirection: 'row', overflow: 'auto' }}>
                         <h3 style={{ minWidth: 180 }}>
                             {formatFeature(key, formatDict)}
                         </h3>
                         <NumberLine
                             key={innerIndex}
-                            explanation={totalExplanations[currentExplanationIndex]}
+                            explanation={totalExplanations[explanations.length === 1 ? 0 : currentExplanationIndex]}
                             i={innerIndex}
                             id={`number-line-container-${currentExplanationIndex}-${innerIndex}`}
                         />
@@ -34,8 +40,8 @@ const ExplanationList = ({ explanations, totalExplanations, featureDict, formatD
             {/* Next and Previous buttons */}
             {explanations.length > 1 && (
                 <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: 10 }}>
-                    <button onClick={handlePrevious} disabled={currentExplanationIndex === 0}>Previous</button>
-                    <button onClick={handleNext} disabled={currentExplanationIndex === explanations.length - 1}>Next</button>
+                    <button className="cycle-button" onClick={handlePrevious} disabled={currentExplanationIndex === 0}>Previous</button>
+                    <button className="cycle-button" onClick={handleNext} disabled={currentExplanationIndex === explanations.length - 1}>Next</button>
                 </div>
             )}
         </div>
