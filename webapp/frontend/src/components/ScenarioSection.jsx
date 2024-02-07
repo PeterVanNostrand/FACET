906e5eb3f0dfb4104bd2db0f1ef9a6bb9dcfc156
Randy Huang <huangyulu.zh@gmail.com>
2024-02-07 02:26:19 -0500

    style selected scenario

--- webapp/frontend/src/components/ScenarioSection.jsx
+++ webapp/frontend/src/components/ScenarioSection.jsx
@@ -1,22 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import close from '../../icons/close.svg';
 
 const ScenarioSection = ({ savedScenarios, setSavedScenarios, setCurrentExplanationIndex, setSelectedInstance }) => {
+    const [selectedScenarioIndex, setSelectedScenarioIndex] = useState(null);
 
-    const handleScenarioChange = (scenario) => {
-        console.log("Scenario changed");
+    const handleScenarioChange = (scenario, index) => {
         setSelectedInstance(scenario.values)
         setCurrentExplanationIndex(scenario.explanationIndex);
+        setSelectedScenarioIndex(index);
     }
 
     const deleteScenario = (index) => {
         let newScenarios = savedScenarios.slice();
         newScenarios.splice(index, 1);
         setSavedScenarios(newScenarios);
+        if (selectedScenarioIndex === index) {
+            setSelectedScenarioIndex(null);
+        }
     }
 
     const clearScenarios = () => {
         setSavedScenarios([]);
+        setSelectedScenarioIndex(null); // Reset selected index when scenarios are cleared
     }
 
     return (
@@ -28,7 +33,11 @@ const ScenarioSection = ({ savedScenarios, setSavedScenarios, setCurrentExplanat
                 :
                 <div className="tab-list">
                     {savedScenarios.map((scenario, index) => (
-                        <div key={index} className="tab" onClick={() => { handleScenarioChange(scenario) }}>
+                        <div
+                            key={index}
+                            className={`tab ${selectedScenarioIndex === index ? 'selected' : ''}`}
+                            onClick={() => { handleScenarioChange(scenario, index) }}
+                        >
                             <p>
                                 Scenario {scenario.scenarioID}.{scenario.explanationIndex + 1}
                             </p>
